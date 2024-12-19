e3ddb1a6ee85e9aaff42a85f8c7a1b2a1eb873dc
RicardoYalico <u201920140@upc.edu.pe>
2024-12-18 20:26:17 -0500

    Word Capitalization

--- Program.cs
+++ Program.cs
@@ -5,24 +5,13 @@ class Program
 {
     static void Main()
     {   
-        List<int> numbers = new() {11, 9, 3, 12, 15, 5, 2,3, 3};
-        List<int> output = new List<int>();
-        
+        string input = Console.ReadLine();
 
-        for (int i = 0; i < numbers.Count; i++)
-        {
-            
-            for (int j = 0; j < numbers.Count-1; j++)
-            {
-                if(numbers[j]>numbers[j+1]){
-                    int temp = numbers[j];
-                    numbers[j] = numbers[j+1];
-                    numbers[j+1] = temp;
-                }
-            }
+        if(input.Length <= Math.Pow(10,3)){
+            string output = input[0].ToString().ToUpper() + input.Remove(0,1);
+
+            Console.WriteLine(output);
         }
-
-        Console.WriteLine(string.Join(",",numbers));
 
     }
 }
--- obj/Debug/net6.0/demo01.AssemblyInfo.cs
+++ obj/Debug/net6.0/demo01.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("demo01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ba61bba265218c45b4ca971fe68cb05df0caabf")]
 [assembly: System.Reflection.AssemblyProductAttribute("demo01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("demo01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
